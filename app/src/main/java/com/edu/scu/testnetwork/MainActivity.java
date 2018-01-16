65305dfe4bb5a48502f353fdc8936c5e477b3d2b
liumolong <185469161@qq.com>
2018-01-17 00:08:00 +0800

    XX

--- app/src/main/java/com/edu/scu/testnetwork/MainActivity.java
+++ app/src/main/java/com/edu/scu/testnetwork/MainActivity.java
@@ -2,6 +2,7 @@ package com.edu.scu.testnetwork;
 
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button button1;//发起请求
@@ -67,13 +69,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("101.200.89.170",8080));
+                        Properties pop = System.getProperties();
+                        String proxyHost ="124.193.33.233";
+                        String proxyPort="3128";
+                        pop.put("proxySet","true");
+                        pop.put("proxyHost",proxyHost);
+                        pop.put("proxyPort",proxyPort);
+                        HttpURLConnection connection=null;
+                        //Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("124.193.33.233",3128));
                         try {
-                            HttpURLConnection connection = (HttpURLConnection) new URL("http://www.baidu.com/").openConnection();
-                            connection.setConnectTimeout(6000);
-                            connection.setReadTimeout(6000);
+                            connection = (HttpURLConnection) new URL("http://www.baidu.com/").openConnection();
+                            connection.setRequestMethod("GET");
+                            connection.setInstanceFollowRedirects(false);
+                            connection.setConnectTimeout(10000);
+                            connection.setReadTimeout(10000);
                             connection.setUseCaches(false);
+                            connection.connect();
+                            Log.d("信息－－－－－－", String.valueOf(connection.getContent()));
+                            Log.d("地址", connection.getHeaderField("Location"));
                             Log.d("网络请求码－－－－－－", String.valueOf(connection.getResponseCode()));
+
 
                             if (connection.getResponseCode() == 200) {
                                 msg.what=0;
@@ -85,6 +100,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             }
                         } catch (IOException e) {
                             e.printStackTrace();
+                        }finally {
+                            if (connection !=null){
+                                connection.disconnect();
+                            }
                         }
                     }
                 }).start();
@@ -103,3 +122,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 }
+/*
+public static void main(String[] args) {
+    	try {
+    		StringBuffer buffer = new StringBuffer();
+
+            String url = "http://localhost:8080/istock/login?u=name&p=pass";
+            System.out.println("访问地址:" + url);
+
+            //发送get请求
+            URL serverUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
+            conn.setRequestMethod("GET");
+            //必须设置false，否则会自动redirect到重定向后的地址
+            conn.setInstanceFollowRedirects(false);
+            conn.addRequestProperty("Accept-Charset", "UTF-8;");
+            conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.8) Firefox/3.6.8");
+            conn.addRequestProperty("Referer", "http://matols.com/");
+            conn.connect();
+
+            //判定是否会进行302重定向
+            if (conn.getResponseCode() == 302) {
+                //如果会重定向，保存302重定向地址，以及Cookies,然后重新发送请求(模拟请求)
+                String location = conn.getHeaderField("Location");
+                String cookies = conn.getHeaderField("Set-Cookie");
+
+	            serverUrl = new URL(location);
+	            conn = (HttpURLConnection) serverUrl.openConnection();
+	            conn.setRequestMethod("GET");
+	            conn.setRequestProperty("Cookie", cookies);
+	            conn.addRequestProperty("Accept-Charset", "UTF-8;");
+	            conn.addRequestProperty("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.2.8) Firefox/3.6.8");
+	            conn.addRequestProperty("Referer", "http://matols.com/");
+	            conn.connect();
+	            System.out.println("跳转地址:" + location);
+            }
+
+            //将返回的输入流转换成字符串
+            InputStream inputStream = conn.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String str = null;
+            while ((str = bufferedReader.readLine()) != null) {
+                buffer.append(str);
+            }
+            bufferedReader.close();
+            inputStreamReader.close();
+            // 释放资源
+            inputStream.close();
+            inputStream = null;
+
+            System.out.println(buffer.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+ */
